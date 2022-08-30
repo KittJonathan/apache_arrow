837653c17bd80e48618b1681944525ccfd07c693
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-08-30 14:19:40 +0200

    update

--- R/apache_arrow_in_r.R
+++ R/apache_arrow_in_r.R
@@ -19,7 +19,7 @@ library(ggplot2)
 library(ggrepel)
 library(sf)
 
-# Download and unzip the data ----
+# Download and unzip the "tiny" data ----
 
 download.file(
   url = "https://github.com/djnavarro/arrow-user2022/releases/download/v0.1/nyc-taxi-tiny.zip",
@@ -30,3 +30,22 @@ unzip(
   zipfile = here::here("data/nyc-taxi-tiny.zip"), 
   exdir = here::here("data")
 )
+
+# The full NYC taxi data set ----
+
+copy_files(
+  from = s3_bucket("ursa-labs-taxi-data-v2"),
+  to = "data/nyc-taxi"
+)
+
+download.file(
+  url = "https://github.com/djnavarro/arrow-user2022/releases/download/v0.1/nyc-taxi-tiny.zip",
+  destfile = here::here("data/nyc-taxi-tiny.zip")
+)
+
+unzip(
+  zipfile = here::here("data/nyc-taxi-tiny.zip"), 
+  exdir = here::here("data")
+)
+
+nyc_taxi <- open_dataset("data/nyc-taxi-tiny/")
