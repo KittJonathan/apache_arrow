e1128883892d1ab53f7ab9310a0204dfbc6adfd2
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-08-30 09:47:06 +0200

    initiated repo

---
+++ apache_arrow_in_r.R
@@ -0,0 +1,10 @@
+# Apache Arrow in R workshop tutorial
+# last updated 2022-08-30
+
+# Quick start guide ----
+
+# Download a copy of the git repo
+usethis::create_from_github(
+  repo_spec = "djnavarro/arrow-user2022", 
+  destdir="../"
+)
